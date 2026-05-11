5ff58125567ec943eabb01b0b0be23fb6d1f6915
MaxGhi8 <massimiliano.ghiotto01@universitadipavia.it>
2026-05-11 15:16:25 +0200

    improve VTK savings

--- examples/tutorial_basis.cpp
+++ examples/tutorial_basis.cpp
@@ -2,13 +2,66 @@
 
     @brief Tutorial on constructing and visualizing B-spline bases in 1D.
 
-    Author(s): Massimiliano Ghiotto
+    Author(s): Massimiliano Ghiotto, Gemini CLI
 */
 
 #include <iostream>
 #include <gismo.h>
 
 using namespace gismo;
+
+/**
+ * @brief Helper to write all basis functions of a 1D basis to a single VTP file.
+ * 
+ * We use gsMesh to collect all functions as separate lines in a single geometry.
+ */
+void writeBasisVTP(const gsBSplineBasis<>& basis, const std::string& filename)
+{
+    gsMesh<> mesh;
+    const index_t npts = 1000;
+    
+    // Sample the basis functions over the domain
+    gsMatrix<> samples = gsPointGrid(basis.support(), npts);
+    gsMatrix<> values = basis.eval(samples);
+
+    // For each basis function, add a "line" to the mesh
+    for (index_t i = 0; i < basis.size(); ++i)
+    {
+        index_t firstV = mesh.numVertices();
+        for (index_t j = 0; j < samples.cols(); ++j)
+        {
+            // Create a 2D plot: (x, value, 0)
+            mesh.addVertex(samples(0, j), values(i, j), 0.0);
+            if (j > 0)
+                mesh.addEdge(firstV + j - 1, firstV + j);
+        }
+    }
+    
+    // gsWriteParaview for gsMesh creates a single .vtp file
+    gsWriteParaview(mesh, filename);
+}
+
+/**
+ * @brief Helper to write a single 1D spline function to a single VTP file.
+ */
+void writeSplineVTP(const gsBSpline<>& spline, const std::string& filename)
+{
+    gsMesh<> mesh;
+    const index_t npts = 1000;
+    
+    gsMatrix<> samples = gsPointGrid(spline.parameterRange(), npts);
+    gsMatrix<> values = spline.eval(samples);
+
+    for (index_t j = 0; j < samples.cols(); ++j)
+    {
+        // For scalar function, values has 1 row
+        mesh.addVertex(samples(0, j), values(0, j), 0.0);
+        if (j > 0)
+            mesh.addEdge(j - 1, j);
+    }
+    
+    gsWriteParaview(mesh, filename);
+}
 
 int main(int argc, char* argv[])
 {
@@ -39,12 +92,11 @@ int main(int argc, char* argv[])
         gsInfo << "Number of elements: " << basis.numElements() << "\n";
         gsInfo << "Number of basis functions: " << basis.size() << "\n";
 
-        // Write to ParaView
         if (output != "")
         {
             std::string filename = output + "_basis_p" + util::to_string(p);
-            gsInfo << "Writing to ParaView file: " << filename << ".pvd\n";
-            gsWriteParaview(basis, filename, 1000);
+            gsInfo << "Writing all basis functions to: " << filename << ".vtp\n";
+            writeBasisVTP(basis, filename);
         }
     }
 
@@ -73,8 +125,8 @@ int main(int argc, char* argv[])
     if (output != "")
     {
         std::string filename = output + "_basis_p2_mult2";
-        gsInfo << "Writing to ParaView file: " << filename << ".pvd\n";
-        gsWriteParaview(basis_mult, filename, 1000);
+        gsInfo << "Writing all basis functions to: " << filename << ".vtp\n";
+        writeBasisVTP(basis_mult, filename);
     }
 
 
@@ -108,18 +160,18 @@ int main(int argc, char* argv[])
     if (output != "")
     {
         std::string filename = output + "_my_function";
-        gsInfo << "Writing function to ParaView file: " << filename << ".pvd\n";
-        gsWriteParaview(spline, filename, 1000);
+        gsInfo << "Writing the single function to: " << filename << ".vtp\n";
+        writeSplineVTP(spline, filename);
     }
 
     if (output == "")
     {
-        gsInfo << "\nDone. No output created, re-run with --output <filename> to get ParaView "
+        gsInfo << "\nDone. No output created, re-run with --output <prefix> to get ParaView "
                   "files containing the solution.\n";
     }
     else
     {
-        gsInfo << "\nDone. Open the " << output << "*.pvd files in ParaView to see the results!\n";
+        gsInfo << "\nDone. Open the " << output << "*.vtp files in ParaView to see the results!\n";
     }
 
     return 0;
